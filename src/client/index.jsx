72f4f34fe41866866d6e331ddf3193525eca612c
shea12 <cshearun12@gmail.com>
2017-09-18 17:02:44 -0500

    toggle render functionality

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -6,8 +6,8 @@ import App from './App'
 /* eslint-disable */
 console.log('Hey geography nerds!')
 /* eslint-enable */
-ReactDOM.render(<App />, document.getElementById('root'))
 
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // render((
 //   <Router history={browserHistory}> 
