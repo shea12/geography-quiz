7f4e6db5150c962777ae09c14a60e260b42ceb2e
shea12 <cshearun12@gmail.com>
2017-09-18 15:00:14 -0500

    update routes for express server

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
 import App from './App'
 
 /* eslint-disable */
 console.log('Hey geography nerds!')
 /* eslint-enable */
 ReactDOM.render(<App />, document.getElementById('root'))
+
+
+// render((
+//   <Router history={browserHistory}> 
+//     <Route path="/" component={App}></Route>
+//   </Router>
+// ), document.getElementById('app'));
