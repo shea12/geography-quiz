d2a288c95f979e562b3ed5b9f44bb33e30394e0c
shea12 <cshearun12@gmail.com>
2017-09-18 20:59:16 -0500

    add new controllers

---
+++ src/server/controllers/countriesController.js
@@ -0,0 +1,37 @@
+const db = require('../schemas.js')
+
+const Countries = db.countries
+
+module.exports = {
+  // description: Returns the number of states in a country
+  // params: { country: abbrv of country ex: 'US' }
+  getNumberOfStates: (req, res) => {
+    return Countries.findOne({
+      abbrv: req.params.country,
+    })
+      .then(country => {
+        res.status(200).send({ numStates: country.numStates })
+      })
+      .catch(error => {
+        /* eslint-disable */
+        console.log('ERROR: ', error)
+        /* eslint-enable */
+        res.status(200).send([])
+      })
+  },
+
+  getCountriesOfContinent: (req, res) => {
+    return Countries.find({
+      continent: req.params.continent,
+    })
+      .then(country => {
+        res.status(200).send({ countries: country })
+      })
+      .catch(error => {
+        /* eslint-disable */
+        console.log('ERROR: ', error)
+        /* eslint-enable */
+        res.status(200).send([])
+      })
+  },
+}
--- src/server/schemas.js
+++ src/server/schemas.js
@@ -6,9 +6,7 @@ const Schema = mongoose.Schema
 const continentSchema = new Schema({
   abbrv: String, // two letter continent abbreviation
   name: String, // continent name
-  lon: Number, // longitude
-  lat: Number, // latitude
-  zoom: Number, // zoom level
+  lonlatzoom: Array, // longitude latitude zoom level
   numCountries: Number, // number of countries in continent
 })
 
@@ -16,23 +14,17 @@ const countrySchema = new Schema({
   abbrv: String, // two letter country abbreviation
   name: String, // country name
   continent: String, // continent name
-  lon: Number, // longitude
-  lat: Number, // latitude
-  zoom: Number, // zoom level
+  lonlatzoom: Array, // longitude latitude zoom level
   numStates: Number, // number of states in country
   cap: String, // capital of country
   leader: String, // leader of country
 })
 
 const stateSchema = new Schema({
+  country: String, // country name
   abbrv: String, // two/three letter state abbreviation
   name: String, // state name
-  country: String, // country name
-  lon: Number, // longitude
-  lat: Number, // latitude
-  zoom: Number, // zoom level
   cap: String, // capital of state
-  gov: String, // name of governor
 })
 
 // haven't created this functionality yet, looking forward to it though
@@ -51,6 +43,6 @@ const userSchema = new Schema({
 module.exports = {
   continents: mongoose.model('continents', continentSchema),
   country: mongoose.model('country', countrySchema),
-  state: mongoose.model('state', stateSchema),
+  states: mongoose.model('state', stateSchema),
   user: mongoose.model('user', userSchema),
 }
