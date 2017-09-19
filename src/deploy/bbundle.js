a715ecf5fca24b59edad37a0d1f066fa29703e32
shea12 <cshearun12@gmail.com>
2017-09-19 09:04:27 -0500

    bbundle

--- src/deploy/bbundle.js
+++ src/deploy/bbundle.js
@@ -80842,7 +80842,6 @@ module.exports = CODES;
 var KEYS = {
   access_token: 'pk.eyJ1IjoiY2FzaGVhIiwiYSI6ImNqNnd3dm9uZTE5Y2wzM3RiaXJxYng1NHAifQ.nEu0dapbPoEPUe4bEYaq7Q',
   style_key: 'mapbox://styles/cashea/cj76hgeil5cz72sqfmglle126'
-
 };
 
 module.exports = KEYS;
