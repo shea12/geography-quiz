83315af92abfd248bf1f48f7674f0ed7b60dbf59
shea12 <cshearun12@gmail.com>
2017-09-19 18:48:18 -0500

    fix middleware conflict

--- src/server/middleware.js
+++ src/server/middleware.js
@@ -1,15 +1,10 @@
 const bodyParser = require('body-parser')
 const path = require('path')
 
-<<<<<<< HEAD
 // const db = require(`${__dirname}/schemas.js`)
 
-module.exports = function (app, express) {
-  // console.log('middleware!')
-=======
 module.exports = function(app, express) {
-  console.log('middleware!')
->>>>>>> b628b9b3e86095af18024323cd35657c3e220c0f
+  // console.log('middleware!')
   app.use(express.static(path.join(__dirname, '../../src/deploy')))
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
