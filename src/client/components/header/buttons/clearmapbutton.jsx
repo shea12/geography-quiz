d933ebf1bebc65ab098c9ad860caeb5ffe91466f
shea12 <cshearun12@gmail.com>
2017-09-27 11:40:31 -0500

    comment out, no longer in use, in menu instead

--- src/client/components/header/buttons/clearmapbutton.jsx
+++ src/client/components/header/buttons/clearmapbutton.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import RaisedButton from 'material-ui/RaisedButton'
-import PropTypes from 'prop-types'
+// not in use
 
-// LINT: component should be written as a pure function
-export default class ClearMapButton extends React.Component {
-  render() {
-    return (
-      <RaisedButton
-        style={{
-          position: 'relative',
-          marginTop: '48%',
-          zIndex: 2,
-          marginLeft: '3%',
-        }}
-        onClick={
-          () => this.props.handleClearMap()
-        }
-      >
-        Clear Map
-      </RaisedButton>
-    )
-  }
-}
+// import React from 'react'
+// import RaisedButton from 'material-ui/RaisedButton'
+// import PropTypes from 'prop-types'
 
-ClearMapButton.propTypes = {
-  handleClearMap: PropTypes.func.isRequired,
-}
+// // LINT: component should be written as a pure function
+// export default class ClearMapButton extends React.Component {
+//   render() {
+//     return (
+//       <RaisedButton
+//         style={{
+//           position: 'relative',
+//           marginTop: '48%',
+//           zIndex: 2,
+//           marginLeft: '3%',
+//         }}
+//         onClick={
+//           () => this.props.handleClearMap()
+//         }
+//       >
+//         Clear Map
+//       </RaisedButton>
+//     )
+//   }
+// }
+
+// ClearMapButton.propTypes = {
+//   handleClearMap: PropTypes.func.isRequired,
+// }
