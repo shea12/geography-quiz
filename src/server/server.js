3bd77870ad9ad3e61a7b36ec3300d689c4c4d642
shea12 <cshearun12@gmail.com>
2017-09-29 11:31:44 -0500

    rearrange to create express only after db connection established

--- src/server/server.js
+++ src/server/server.js
@@ -1,28 +1,50 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
 const mongoURI = require(`${__dirname}/dbkey.js`)
-
-const app = express()
+var dbServer  = process.env.DB_ENV || 'primary'
 
 mongoose.connect(mongoURI, {
   useMongoClient: true,
 })
 
-const port = process.env.PORT || 8081
-require('./middleware.js')(app, express)
-require('./routes.js')(app, express)
+mongoose.connection.on('connected', function(ref) {
+  console.log('Connected to DB: ' + dbServer);
+  
+  const app = express()
+  const port = process.env.PORT || 8081
+  require('./middleware.js')(app, express)
+  require('./routes.js')(app, express)
 
-app.listen(port, function () {
-  /* eslint-disable */
-  console.log('App running on port: ', port)
-  /* eslint-enable */
+  app.listen(port, function () {
+    /* eslint-disable */
+    console.log('Listening on port: ', port)
+    /* eslint-enable */
+  })
 })
+
+mongoose.connection.on('error', function(error) {
+  console.error('Error connecting to DB: ' + dbServer + '. Error: ', error)
+})
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Disconnected DB: ' + dbServer)
+})
+
+const gracefulExit = function() { 
+  mongoose.connection.close(function () {
+    console.log('Node process ended, closed DB: ' + dbServer)
+    process.exit(0)
+  })
+}
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit)
 
 /*
 deployment process:
+  remember! <script> window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () {} </script>
   git push heroku master
   heroku open
   heroku logs --tail
 
+CL mLab: 
+  mongo ds139984.mlab.com:39984/geo-quiz -u {username} -p {password}
 */
